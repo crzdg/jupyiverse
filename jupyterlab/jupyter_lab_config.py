7bd9d88ce702b70d47a8054aef1eb2b90dacca4b
crzdg <reto@fambarm.ch>
2023-04-05 13:42:30 +0200

    Refactor images
    We handle now the different versions, runtimes and kernel specs.
    according to their pip-requirements.

--- jupyterlab/jupyter_lab_config.py
+++ jupyterlab/jupyter_lab_config.py
@@ -1,3 +1,4 @@
 c.JupyterLabTemplates.template_dirs = ["notebook_templates"]
 c.JupyterLabTemplates.include_default = True
 c.JupyterLabTemplates.include_core_paths = True
+c.KernelSpecManager.ensure_native_kernel = False
